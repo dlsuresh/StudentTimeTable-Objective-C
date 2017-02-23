6b7b168e8b59afc4de2ba2fec93ec7bd22048bdc
dlsuresh <dl.suresh6@gmail.com>
2017-02-23 12:30:05 +0530

    Initial Commit

---
+++ StudentTimeTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudentTimeTable
+//
+//  Created by Suresh on 2/23/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
